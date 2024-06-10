X11Libre/xf86-video-ark

commit 71da1466eee5c9db6a5150ebfce9ee54a00e96be

Raise minimum supported Xserver version to 1.18 (ABI_VIDEODRV_VERSION 20.0)
Already effectively required by use of XNFcallocarray() introduced in
xorg/xserver@b96dc999 - xserver-1.18.0, released in Nov. 2015.

Allows dropping remnants of code for XAA and pre-pciaccess X servers

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-ark/-/merge_requests/6>

src/ark.h
@@ -7,9 +7,6 @@
 
 #include "xf86.h"
 #include "xf86Pci.h"
-#ifdef HAVE_XAA_H
-#include "xaa.h"
-#endif
 #include "vgaHW.h"
 
 #include "compat-api.h"
@@ -77,8 +74,6 @@ typedef struct _ARKRec {
 
 #define	ZOOMDAC		0x404
 #define ATT490		0x101
-
-Bool ARKAccelInit(ScreenPtr pScreen);
 
 #define rdinx(port, ind)	(outb((port), (ind)), inb((port) + 1))
 #define wrinx(port, ind, val)						\

configure.ac
@@ -55,7 +55,7 @@ AC_SUBST(moduledir)
 XORG_DRIVER_CHECK_EXT(DPMSExtension, xextproto)
 
 # Obtain compiler/linker options for the driver dependencies
-PKG_CHECK_MODULES(XORG, [xorg-server >= 1.0.99.901] xproto fontsproto $REQUIRED_MODULES)
+PKG_CHECK_MODULES(XORG, [xorg-server >= 1.18] xproto fontsproto $REQUIRED_MODULES)
 
 # Checks for libraries.
 SAVE_CPPFLAGS="$CPPFLAGS"
@@ -69,23 +69,6 @@ if test "x$XSERVER_LIBPCIACCESS" = xyes; then
     PKG_CHECK_MODULES([PCIACCESS], [pciaccess >= 0.12.901])
 fi
 AM_CONDITIONAL(XSERVER_LIBPCIACCESS, test "x$XSERVER_LIBPCIACCESS" = xyes)
-AC_ARG_ENABLE(xaa,
-              AS_HELP_STRING([--enable-xaa],
-                             [Enable legacy X Acceleration Architecture (XAA) [default=auto]]),
-              [XAA="$enableval"],
-              [XAA=auto])
-if test "x$XAA" != xno; then
-        save_CFLAGS=$CFLAGS
-        save_CPPFLAGS=$CPPFLAGS
-        CFLAGS=$XORG_CFLAGS
-        CPPFLAGS="$XORG_CFLAGS"
-        AC_CHECK_HEADERS([xaa.h], XAA=yes, XAA=no)
-        CFLAGS=$save_CFLAGS
-        CPPFLAGS=$save_CPPFLAGS
-fi
-AC_MSG_CHECKING([whether to include XAA support])
-AM_CONDITIONAL(XAA, test "x$XAA" = xyes)
-AC_MSG_RESULT([$XAA])
 
 # Checks for library functions
 # We don't use strlcat, strlcpy, or timingsafe_memcmp, but check to quiet

src/Makefile.am
@@ -33,7 +33,6 @@ ark_drv_ladir = @moduledir@/drivers
 
 ark_drv_la_SOURCES = \
          compat-api.h \
-         ark_accel.c \
          ark_driver.c \
          ark.h \
          ark_reg.h

src/ark_accel.c
@@ -1,233 +1,0 @@
-/*
- *      Copyright 2000  Ani Joshi <ajoshi@unixbox.com>
- *
- *      XFree86 4.x driver for ARK Logic chipset
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that copyright
- * notice and this permission notice appear in supporting documentation and
- * that the name of Ani Joshi not be used in advertising or
- * publicity pertaining to distribution of the software without specific,
- * written prior permission.  Ani Joshi makes no representations
- * about the suitability of this software for any purpose.  It is provided
- * "as-is" without express or implied warranty.
- *
- * ANI JOSHI DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
- * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
- * EVENT SHALL ANI JOSHI BE LIABLE FOR ANY SPECIAL, INDIRECT OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
- * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
- * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- *
- *
- *      Based on the 3.3.x driver by:
- *              Harm Hanemaayer <H.Hanemaayer@inter.nl.net>
- *
- */
-
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include <X11/Xarch.h>
-#include "xf86.h"
-#include "xf86_OSproc.h"
-#include "compiler.h"
-
-#include "ark.h"
-#include "ark_reg.h"
-
-#ifdef HAVE_XAA_H
-static int curx, cury, cmd_flags;
-
-static void ARKSync(ScrnInfoPtr pScrn)
-{
-	unsigned long port = 0x3cb;
-#if ABI_VIDEODRV_VERSION < 12
-	port += pScrn->domainIOBase + 0x3cb;
-#endif
-
-	for (;;) {
-		if (!(inb(port) & 0x40))
-			break;
-	}
-}
-
-
-static void ARKSetupForSolidFill(ScrnInfoPtr pScrn, int color,
-				 int rop, unsigned int planemask)
-{
-	ARKPtr pARK = ARKPTR(pScrn);
-
-	OUTREG16(FG_COLOR, color);
-	/* ARK color mix matches X raster-ops */
-	OUTREG16(COLOR_MIX_SEL, (rop | (rop << 8)));
-	switch (pScrn->bitsPerPixel) {
-		case 8:
-			if ((planemask & 0xff) == 0xff)
-				cmd_flags = DISABLE_PLANEMASK;
-			else {
-				OUTREG16(WRITE_PLANEMASK, planemask);
-				cmd_flags = 0;
-			}
-			break;
-		case 16:
-			if ((planemask & 0xffff) == 0xffff)
-				cmd_flags = DISABLE_PLANEMASK;
-			else {
-				OUTREG16(WRITE_PLANEMASK, planemask);
-				cmd_flags = 0;
-			}
-			break;
-		case 32:
-			OUTREG16(FG_COLOR_HI, color >> 16);
-			if ((planemask & 0xffffff) == 0xffffff)
-				cmd_flags = DISABLE_PLANEMASK;
-			else {
-				OUTREG16(WRITE_PLANEMASK, planemask);
-				cmd_flags = 0;
-			}
-			break;
-	}
-
-	curx = cury = -1;
-}
-
-
-static void ARKSubsequentSolidFillRect(ScrnInfoPtr pScrn, int x,
-				       int y, int w, int h)
-{
-	ARKPtr pARK = ARKPTR(pScrn);
-	int dst_addr;
-
-	OUTREG(WIDTH, ((h - 1) << 16) | (w - 1));
-	if (x != curx || y != cury) {
-		dst_addr = y * pScrn->displayWidth + x;
-		OUTREG(DST_ADDR, dst_addr);
-		curx = x;
-		cury = y;
-	}
-	OUTREG16(COMMAND, SELECT_BG_COLOR | SELECT_FG_COLOR |
-			  STENCIL_ONES | DISABLE_CLIPPING | BITBLT |
-			  cmd_flags);
-	cury += h;
-}
-
-
-
-static void ARKSetupForScreenToScreenCopy(ScrnInfoPtr pScrn, int xdir,
-					  int ydir, int rop, unsigned int planemask,
-					  int trans_color)
-{
-	ARKPtr pARK = ARKPTR(pScrn);
-
-	cmd_flags = 0;
-	if (trans_color != -1) {
-		if (pScrn->bitsPerPixel <= 16)
-			OUTREG16(TRANS_COLOR, trans_color);
-		else {
-			OUTREG16(TRANS_COLOR, trans_color & 0xffff);
-			OUTREG16(TRANS_COLOR_HI, trans_color >> 16);
-		}
-		cmd_flags = STENCIL_GENERATED;
-		OUTREG16(COLOR_MIX_SEL, rop | 0x0500);
-	} else {
-		OUTREG16(COLOR_MIX_SEL, rop | (rop << 8));
-	}
-
-	if (ydir < 0)
-		cmd_flags |= UP;
-	if (xdir < 0)
-		cmd_flags |= LEFT;
-
-	/* yes, quite ugly */
-	if ((pScrn->bitsPerPixel == 8 && (planemask & 0xff) == 0xff) ||
-	    (pScrn->bitsPerPixel == 16 && (planemask & 0xffff) == 0xffff) ||
-	    (pScrn->bitsPerPixel == 32 && (planemask & 0xffffff) == 0xffffff))
-		cmd_flags |= DISABLE_PLANEMASK;
-	else
-		OUTREG16(WRITE_PLANEMASK, planemask);
-
-}
-
-
-
-static void ARKSubsequentScreenToScreenCopy(ScrnInfoPtr pScrn,
-					    int x1, int y1,
-					    int x2, int y2,
-					    int w, int h)
-{
-	ARKPtr pARK = ARKPTR(pScrn);
-	int src_addr, dst_addr;
-
-	if (cmd_flags & UP) {
-		src_addr = (y1 + h - 1) * pScrn->displayWidth;
-		dst_addr = (y2 + h - 1) * pScrn->displayWidth;
-	} else {
-		src_addr = y1 * pScrn->displayWidth;
-		dst_addr = y2 * pScrn->displayWidth;
-	}
-	if (cmd_flags & LEFT) {
-		src_addr += x1 + w - 1;
-		dst_addr += x2 + w - 1;
-	} else {
-		src_addr += x1;
-		dst_addr += x2;
-	}
-
-	OUTREG(SRC_ADDR, src_addr);
-	OUTREG(DST_ADDR, dst_addr);
-	OUTREG(WIDTH, ((h - 1) << 16) | (w - 1));
-	OUTREG16(COMMAND, BG_BITMAP | FG_BITMAP | DISABLE_CLIPPING |
-			  BITBLT | cmd_flags);
-}
-
-#endif
-
-Bool ARKAccelInit(ScreenPtr pScreen)
-{
-#ifdef HAVE_XAA_H
-	ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
-	ARKPtr pARK = ARKPTR(pScrn);
-	XAAInfoRecPtr pXAA;
-
-	if (!(pXAA = XAACreateInfoRec()))
-		return FALSE;
-
-	pXAA->Flags = LINEAR_FRAMEBUFFER;
-
-	pXAA->Sync = ARKSync;
-	pXAA->SetupForSolidFill = ARKSetupForSolidFill;
-	pXAA->SubsequentSolidFillRect = ARKSubsequentSolidFillRect;
-	pXAA->ScreenToScreenCopyFlags = 0;
-	pXAA->SetupForScreenToScreenCopy = ARKSetupForScreenToScreenCopy;
-	pXAA->SubsequentScreenToScreenCopy = ARKSubsequentScreenToScreenCopy;
-
-	OUTREG16(COLOR_MIX_SEL, 0x0303);
-	if (pARK->Chipset == PCI_CHIP_1000PV) {
-		OUTREG16(WRITE_PLANEMASK, 0xffff);
-		OUTREG16(TRANS_COLOR_MSK, 0xffff);
-	} else {
-		OUTREG16(TRANS_COLOR, 0xffff);
-		OUTREG16(TRANS_COLOR, 0xffffffff >> 16);
-	}
-	if (pARK->Chipset == PCI_CHIP_1000PV && pScrn->bitsPerPixel == 32) {
-		OUTREG16(STENCIL_PITCH, pScrn->displayWidth * 2);
-		OUTREG16(SRC_PITCH, pScrn->displayWidth * 2);
-		OUTREG16(DST_PITCH, pScrn->displayWidth * 2);
-	} else {
-		OUTREG16(STENCIL_PITCH, pScrn->displayWidth);
-		OUTREG16(SRC_PITCH, pScrn->displayWidth);
-		OUTREG16(DST_PITCH, pScrn->displayWidth);
-	}
-
-	OUTREG16(BITMAP_CONFIG, LINEAR_STENCIL_ADDR | LINEAR_SRC_ADDR |
-				LINEAR_DST_ADDR);
-
-	return XAAInit(pScreen, pXAA);
-#else
-        return FALSE;
-#endif
-}

src/ark_driver.c
@@ -42,15 +42,7 @@
 #include "fb.h"
 #include "ark.h"
 
-#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 6
-#include "xf86Resources.h"
-#endif
-
-#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
-#define PIOOFFSET hwp->PIOOffset
-#else
-#define PIOOFFSET 0
-#endif
+#define PIOOFFSET 0  /* was hwp->PIOOffset in ABI_VIDEODRV_VERSION < 12 */
 
 #include <string.h>
 
@@ -540,14 +532,9 @@ static Bool ARKScreenInit(SCREEN_INIT_ARGS_DECL)
 	xf86SetBackingStore(pScreen);
 
 	if (!pARK->NoAccel) {
-		if (ARKAccelInit(pScreen)) {
-			xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Acceleration enabled\n");
-		} else {
-			xf86DrvMsg(pScrn->scrnIndex, X_ERROR, "Acceleration initialization failed\n");
-			xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Acceleration disabled\n");
-		}
+            xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Acceleration not available\n");
 	} else {
-			xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Acceleration disabled\n");
+            xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Acceleration disabled\n");
 	}
 
 	miDCInitialize(pScreen, xf86GetPointerScreenFuncs());
@@ -1117,11 +1104,9 @@ static void ARKLoadPalette(ScrnInfoPtr pScrn, int numColors,
 			   VisualPtr pVisual)
 {
 	unsigned long isaIOBase = 0;
-#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 12
-	isaIOBase += pScrn->domainIOBase;
-#endif
 	int i, index;
 
+	/* isaIOBase += pScrn->domainIOBase; */
 	for (i=0; i<numColors; i++) {
 		index = indices[i];
 		outb(isaIOBase + 0x3c8, index);

commit 109745d5c7e6982ee0dabbc5a233c1f2667ad5c9

clean remains of removed XAA support
A few lines have been overseen.

Fixes: 71da1466eee5c9db6a5150ebfce9ee54a00e96be
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-video-ark/-/merge_requests/8>

src/ark.h
@@ -45,9 +45,6 @@ typedef struct _ARKRec {
 	unsigned int		Flags;
 	Bool			NoAccel;
 	CARD32			Bus;
-#ifdef HAVE_XAA_H
-	XAAInfoRecPtr		pXAA;
-#endif
 	int			Chipset, ChipRev;
 	int			clock_mult;
 	int			dac_width;